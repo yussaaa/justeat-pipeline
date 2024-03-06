c55ee7bda48992a3d6dd1efe1f9ccd886036177a
yussaaa <yusa.li@mail.utoronto.ca>
2024-03-06 17:40:19 -0500

    code formatting

--- src/model/data_collection.py
+++ src/model/data_collection.py
@@ -2,25 +2,28 @@ from io import StringIO
 import pandas as pd
 import numpy as np
 
-def data_ingestion(SAMPLE_DATA: str) -> (pd.DataFrame, dict):
+
+def data_ingestion(SAMPLE_DATA: str) -> tuple[pd.DataFrame, dict]:
 
     df = pd.read_csv(SAMPLE_DATA)
     # df = pd.read_csv('final_dataset.csv')
     df.dropna(axis=0, inplace=True)
 
-    #unique restaurants
+    # unique restaurants
     restaurants_ids = {}
     list_restaurants_ids = []
-    for a,b in zip(df.restaurant_lat, df.restaurant_lon):
-        id = "{}_{}".format(a,b)
-        restaurants_ids[id] = {"lat": a, "lon":b}
-    for i,key in enumerate(restaurants_ids.keys()):
-        restaurants_ids[key]['id'] = i
+    for a, b in zip(df.restaurant_lat, df.restaurant_lon):
+        id = "{}_{}".format(a, b)
+        restaurants_ids[id] = {"lat": a, "lon": b}
+    for i, key in enumerate(restaurants_ids.keys()):
+        restaurants_ids[key]["id"] = i
 
-    #labeling of restaurants
-    df['restaurant_id']=[restaurants_ids["{}_{}".format(a,b)]['id'] for a,b in zip(df.restaurant_lat, df.restaurant_lon)]
-    
+    # labeling of restaurants
+    df["restaurant_id"] = [
+        restaurants_ids["{}_{}".format(a, b)]["id"]
+        for a, b in zip(df.restaurant_lat, df.restaurant_lon)
+    ]
+
     # # number of unique restaurants
     # len(restaurants_ids)
     return df, restaurants_ids
-    
