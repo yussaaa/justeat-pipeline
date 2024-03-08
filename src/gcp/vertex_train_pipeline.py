e2b7be190beb098789f1aad46ece9ace1dcfa698
yussaaa <yusa.li@mail.utoronto.ca>
2024-03-08 09:44:04 -0500

    Make comment and move the vertex model train helper func

--- src/gcp/vertex_train_pipeline.py
+++ src/gcp/vertex_train_pipeline.py
@@ -7,6 +7,12 @@ BUCKET_NAME = "cloud-ai-platform-4de6a256-deb4-4f4f-8b9f-8595e0183ea1"
 
 
 def main():
+    """This is the sdk version of model training. It's an alternative to the gcloud command line tool.
+        gcloud ai custom-jobs create \
+            --region=${{env.REGION}} \
+            --display-name=${{env.JOB_NAME}} \
+            --config=vertex_run.yaml
+    """
     vertexai.init(project=PROJECT_NAME, staging_bucket=BUCKET_NAME)
 
     worker_pool_specs = [
