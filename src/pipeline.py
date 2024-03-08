fd8bfeb3aca7f2f37090779a727e3d8c00251356
yussaaa <yusa.li@mail.utoronto.ca>
2024-03-07 22:54:40 -0500

    Update pipeline, run on vertex

--- src/pipeline.py
+++ src/pipeline.py
@@ -2,6 +2,11 @@ from model.data_collection import data_ingestion
 from model.feature_generation import generate_features
 from model.training import data_split, trainging_pipeline_run
 
+from google.cloud import storage
+from joblib import dump, load
+
+from google.oauth2 import service_account
+
 
 def run_pipeline(SAMPLE_DATA):
     df, restaurants_ids = data_ingestion(SAMPLE_DATA)
@@ -10,6 +15,24 @@ def run_pipeline(SAMPLE_DATA):
     X_train, X_test, y_train, y_test = data_split(df)
 
     best_model = trainging_pipeline_run(X_train, y_train)
+
+    dump(best_model, "./models/justeat_regrssor.joblib")
+
+    upload_blob(
+        "cloud-ai-platform-4de6a256-deb4-4f4f-8b9f-8595e0183ea1",
+        "./models/justeat_regrssor.joblib",
+        "models/justeat_regrss.joblib",
+    )
+
+
+def upload_blob(bucket_name, source_file_name, destination_blob_name):
+    storage_client = storage.Client(project="citric-program-331111")
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(destination_blob_name)
+
+    blob.upload_from_filename(source_file_name)
+
+    print("File {} uploaded to {}.".format(source_file_name, destination_blob_name))
 
 
 if __name__ == "__main__":
---
+++ src/vertex_train_pipeline.py
@@ -0,0 +1,33 @@
+from google.cloud import aiplatform as vertexai
+
+PROJECT_NAME = "citric-program-331111"
+JOB_NAME = "justeat_regressor_training"
+CUSTOM_CONTAINER_IMAGE_URI = "gcr.io/citric-program-331111/justeat-pipeline"
+BUCKET_NAME = "cloud-ai-platform-4de6a256-deb4-4f4f-8b9f-8595e0183ea1"
+
+
+def main():
+    vertexai.init(project=PROJECT_NAME, staging_bucket=BUCKET_NAME)
+
+    worker_pool_specs = [
+        {
+            "machine_spec": {
+                "machine_type": "n1-standard-8",
+            },
+            "replica_count": 1,
+            "container_spec": {"image_uri": CUSTOM_CONTAINER_IMAGE_URI},
+        }
+    ]
+    print(worker_pool_specs)
+
+    job = vertexai.CustomJob(
+        display_name=f"{JOB_NAME}", worker_pool_specs=worker_pool_specs
+    )
+
+    job.run(
+        sync=False,
+    )
+
+
+if __name__ == "__main__":
+    main()
